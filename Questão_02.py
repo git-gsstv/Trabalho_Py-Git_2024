6a8663e81aa74ed42441e3e178b14a7692d4d6db
git-gsstv <gcoutinho26oliveira@gmail.com>
2024-05-28 20:12:40 -0300

    Adicionada questão 02

---
+++ Questão_02.py
@@ -0,0 +1,10 @@
+# int = int(input("Digite um número inteiro: "))
+varv_int = 10
+varv_float = 3.1415
+varv_str = 'DS1 melhor curso'
+varv_bool = True
+
+print(f'{varv_int}, seu tipo é: {type(varv_int)}')
+print(f'{varv_float}, seu tipo é: {type(varv_float)}')
+print(f'{varv_str}, seu tipo é: {type(varv_str)}')
+print(f'{varv_bool}, seu tipo é: {type(varv_bool)}')
