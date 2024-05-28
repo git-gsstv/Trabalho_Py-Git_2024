685ac5154424109118e9bc04c0c89d9c57383f71
git-gsstv <gcoutinho26oliveira@gmail.com>
2024-05-28 20:11:56 -0300

    Adicionada questão 01

---
+++ Questão_01.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+sobrenome = input("Agora digite seu(s) sobrenome(s): ")
+ultimoNome = input("E seu último nome: ")
+nomeCompleto = nome + " " + sobrenome + " " + ultimoNome
+print(f'Seu nome completo é: {nomeCompleto}')
