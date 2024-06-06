b45587e64fabb3433990ace335b1f507f95eb6b5
git-gsstv <gcoutinho26oliveira@gmail.com>
2024-06-06 16:43:50 -0300

    Adicionada questão 08

---
+++ Questão_08.py
@@ -0,0 +1,16 @@
+def adicao(p,g):
+    return p + g
+def subtracao(p,g):
+    return p - g
+def multiplicacao(p,g):
+    return p * g
+def divisao(p,g):
+    return p / g
+
+
+p = int(input("Digite um número: "))
+g = int(input("Agora digite outro número: "))
+print(f'A soma desses números é: {adicao(p,g)}')
+print(f'A diferença entre números é: {subtracao(p,g)}')
+print(f'O produto desses números resulta em: {multiplicacao(p,g)}')
+print(f'A divisão desses números equivale a: {divisao(p,g)}')
