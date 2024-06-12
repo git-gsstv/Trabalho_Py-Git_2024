2d94b3c8eee0750e0697d049d19054c32157d1d2
git-gsstv <gcoutinho26oliveira@gmail.com>
2024-06-12 10:05:35 -0300

    Modificada questão 08 (uma string)

--- Questão_08.py
+++ Questão_08.py
@@ -11,6 +11,6 @@ def divisao(p,g):
 p = int(input("Digite um número: "))
 g = int(input("Agora digite outro número: "))
 print(f'A soma desses números é: {adicao(p,g)}')
-print(f'A diferença entre números é: {subtracao(p,g)}')
+print(f'A diferença entre esses números é: {subtracao(p,g)}')
 print(f'O produto desses números resulta em: {multiplicacao(p,g)}')
 print(f'A divisão desses números equivale a: {divisao(p,g)}')
